7666e516987f8528bbb9d7626339ba8e6075dc04
user_name <email_id>
2023-06-14 22:21:57 +0400

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+const todoList = []
+
+function addToTodo() {
+    todoList.push(todoInput.value)
+
+    res.innerHTML = ""
+
+    todoList.forEach((todo,i) => {
+        res.innerHTML += `${i+1} ${todo}<br>`
+
+    })
+
+}
